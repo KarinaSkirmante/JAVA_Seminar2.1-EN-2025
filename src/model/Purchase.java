964f59a58d91cc8b17e733bccbb3745556a2dcab
KarinaSkirmante <karina.krinkele@gmail.com>
2025-02-26 16:04:25 +0200

    Add: added Purchase with variables, getters ands setters

---
+++ src/model/Purchase.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class Purchase {
+	// variables, getters, setters, both constructors, toString + other functions(if necessary)
+	private String userCardNr;
+	private ArrayList<Vehicle> shoppingList = new ArrayList<Vehicle>();
+	private LocalDateTime dateTime;
+	
+	public String getUserCardNr() {
+		return userCardNr;
+	}
+	public ArrayList<Vehicle> getShoppingList() {
+		return shoppingList;
+	}
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+	
+	public void setUserCardNr(String inputUserCardNr) {
+		if(inputUserCardNr != null && inputUserCardNr.matches("[A-Z]{3}[0-9]{6}")) {
+			userCardNr = inputUserCardNr;
+		}
+		else
+		{
+			userCardNr = "No card no";
+		}
+	}
+	
+	//no set for shoppinglist because it is initialized as empty list and adding some tractors or busses will be done in next functions
+
+	public void setDateTime() {
+		dateTime = LocalDateTime.now();
+	}
+}
